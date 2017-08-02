bb3e9b8abd1d861a94c34c6256ef6a2585517fb1
Masquerade <masquerade0097@gmail.com>
2017-08-02 08:10:50 +0530

    Fragment for android stomach and legs added

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,6 +21,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -31,6 +32,16 @@ public class AndroidMeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_android_me);
 
         BodyPartFragment headFragment = new BodyPartFragment();
+        BodyPartFragment stomachFragment = new BodyPartFragment();
+        BodyPartFragment legsFragment = new BodyPartFragment();
+
+        headFragment.setImageIds(AndroidImageAssets.getHeads());
+        stomachFragment.setImageIds(AndroidImageAssets.getStomach());
+        legsFragment.setImageIds(AndroidImageAssets.getLegs());
+
+        headFragment.setListIndex(1);
+        stomachFragment.setListIndex(1);
+        legsFragment.setListIndex(1);
 
 //  To make fragment transactions in your activity (such as add, remove, or replace a
 // fragment), you must use APIs from FragmentTransaction. You can get an instance of
@@ -39,8 +50,11 @@ public class AndroidMeActivity extends AppCompatActivity {
 //        The first argument passed to add() is the ViewGroup in which the fragment
 // should be placed, specified by resource ID, and the second parameter is the fragment
 // to add
+
         fragmentManager.beginTransaction()
                 .add(R.id.head_container,headFragment)
+                .add(R.id.stomach_container,stomachFragment)
+                .add(R.id.legs_container,legsFragment)
                 .commit();
 
 
--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -3,19 +3,26 @@ package com.example.android.android_me.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.android.android_me.R;
-import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.List;
 
 /**
  * Created by priyanshu on 1/8/17.
  */
 
 public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = "BodyPartActivity";
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
 
     public BodyPartFragment(){}
 
@@ -29,8 +36,20 @@ public class BodyPartFragment extends Fragment {
         //imageview in the fragment
         ImageView body_part_image = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
-        body_part_image.setImageResource(AndroidImageAssets.getHeads().get(0));
+        if(mImageIds != null){
+            body_part_image.setImageResource(mImageIds.get(mListIndex));
+        }else{
+            Log.v(TAG,"This fragment has null list of list of image id's");
+        }
 
         return rootView;
+    }
+
+    public void setImageIds(List<Integer> imageIds){
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int index){
+        mListIndex = index;
     }
 }
